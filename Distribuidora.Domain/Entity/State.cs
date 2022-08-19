687fdb23fd9e395b568078e6fe2727b3decc0a28
welberoliveira <welberoliveira3@gmail.com>
2022-08-19 13:42:22 -0300

    Envio do teste unitário da Classe State. Envio da Classe State.

---
+++ Distribuidora.Domain/Entity/State.cs
@@ -0,0 +1,40 @@
+﻿namespace Distribuidora.Domain.Entity;
+public class State
+{
+    private static long serialVersionUID = 1L;
+
+    public Guid Codigo => Guid.NewGuid();
+    public String Name { get; set; }
+    public String Abreviation { get; set; }
+
+    public State(string name, string abreviation)
+    {
+        Name = name;
+        Abreviation = abreviation;
+    }
+
+    public int HashCode() => Codigo.GetHashCode();
+    
+    /*
+    É necessário entender para que essa função foi criada, por isso mantive comentada porque 
+    apesar de eu não ter entendido o que ela faz, isso pode ser útil no futuro
+    */
+    /*public bool equals(Object obj)
+    {
+        /*if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Estado other = (Estado)obj;
+        if (Codigo == null)
+        {
+            if (other.codigo != null)
+                return false;
+        }
+        else if (!Codigo.equals(other.codigo))
+            return false;
+        return true;
+    }*/
+}
---
+++ Distribuidora.UnitTests/Domain/Entity/State/StateTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using DomainEntity = Distribuidora.Domain.Entity;
+using FluentAssertions;
+
+namespace Distribuidora.UnitTests.Domain.Entity.State;
+public class StateTest
+{
+    [Fact(DisplayName = nameof(Instaciate))]
+    [Trait("Domain", "State")]
+    public void Instaciate()
+    {
+        //Arrange
+        String Name = "Minas Gerais";
+        String Abreviation = "MG";
+
+        //Act
+        var state = new DomainEntity.State(Name, Abreviation);
+
+        //Assert
+        state.Should().NotBeNull();
+        state.Name.Should().Be(Name);
+        state.Abreviation.Should().Be(Abreviation);
+    }
+}
