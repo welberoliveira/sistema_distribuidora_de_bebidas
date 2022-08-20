2509e7097777addd4de2996e8855b692bb679d8c
welberoliveira <welberoliveira3@gmail.com>
2022-08-20 11:28:09 -0300

    Criado o teste unitário resolve #16

---
+++ Distribuidora.UnitTests/Domain/Entity/City/City.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using DomainEntity = Distribuidora.Domain.Entity;
+using FluentAssertions;
+
+namespace Distribuidora.UnitTests.Domain.Entity.State;
+public class City
+{
+    [Fact(DisplayName = nameof(Instaciate))]
+    [Trait("Domain", "City")]
+    public void Instaciate()
+    {
+        //Arrange
+        String cityName = "Manga";
+        String stateName = "Minas Gerais";
+        String stateAbreviation = "MG";
+
+        //Act
+        var state = new DomainEntity.State(stateName, stateAbreviation);
+        var city = new DomainEntity.City(cityName, state);
+
+        //Assert
+        state.Should().NotBeNull();
+        city.Should().NotBeNull();
+        city.stateName.Should().Be(stateName);
+        city.stateAbreviation.Should().Be(stateAbreviation);
+        city.Name.Should().Be(cityName);
+    }
+}
