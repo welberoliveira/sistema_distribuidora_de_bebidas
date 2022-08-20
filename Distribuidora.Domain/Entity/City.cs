4c12ba39f7586927ea5993da7423d0b43cf339f6
welberoliveira <welberoliveira3@gmail.com>
2022-08-20 11:47:33 -0300

    Criação da entidade City resolve #14

---
+++ Distribuidora.Domain/Entity/City.cs
@@ -0,0 +1,13 @@
+﻿namespace Distribuidora.Domain.Entity;
+public class City
+{
+    public Guid Codigo => Guid.NewGuid();
+    public String Name { get; set; }
+    public State State { get; set; }
+
+    public City(string name, State state)
+    {
+        Name = name;
+        State = state;
+    }
+}
