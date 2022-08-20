1c6d08dc56b6ec2ec371bd2633581681b6867cbe
Salomão Ribeiro <salomaomdm@gmail.com>
2022-08-20 14:37:43 -0300

    Ajuste de PR

--- Distribuidora.Domain/Exceptions/EntityValidationException.cs
+++ Distribuidora.Domain/Exceptions/EntityValidationException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Distribuidora.Domain.Exceptions
+﻿namespace Distribuidora.Domain.Exceptions
 {
     internal class EntityValidationException: Exception
     {
