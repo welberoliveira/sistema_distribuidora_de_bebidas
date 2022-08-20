ec2eaf3956113567f025deb7d9221fcf7f19871e
Salomão Ribeiro <salomaomdm@gmail.com>
2022-08-20 07:19:00 -0300

    Criado classe para validar entity do Domain
    (resolve #18)

---
+++ Distribuidora.Domain/Exceptions/EntityValidationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distribuidora.Domain.Exceptions
+{
+    internal class EntityValidationException: Exception
+    {
+        public EntityValidationException(string? message): base(message)
+        {
+        }
+    }
+}
