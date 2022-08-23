a8c7ec525dc209ba9e4eb278dc9857d619feb0da
welberoliveira <welberoliveira3@gmail.com>
2022-08-23 13:10:40 -0300

    Add files via upload

---
+++ Distribuidora.UnitTests/Domain/Entity/City/CityTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using DomainEntity = Distribuidora.Domain.Entity;
+using FluentAssertions;
+
+namespace Distribuidora.UnitTests.Domain.Entity.State;
+public class CityTest
+{
+    [Fact(DisplayName = nameof(Instaciate))]
+    [Trait("Domain", "City")]
+    public void Instaciate()
+    {
+        //Arrange
+        String cityName = "Manga";
+        String stateName = "Minas Gerais";
+        String stateAbreviation = "MG";
+
+        //Act
+        var state = new DomainEntity.State(stateName, stateAbreviation);
+        var city = new DomainEntity.City(cityName, state);
+
+        //Assert
+        state.Should().NotBeNull();
+        city.Should().NotBeNull();
+        city.Name.Should().Be(cityName);
+    }
+}
