d0feb8ddf1ba1f7ef8cc669b94a9e45691e7d61d
welberoliveira <welberoliveira3@gmail.com>
2022-08-25 13:22:47 -0300

    Criação da Classe Category resolve #33

---
+++ Distribuidora.Domain/Entity/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Distribuidora.Domain.Entity;
+internal class Category
+{
+    public Guid Id => Guid.NewGuid();
+    public String Name { get; set; }
+
+    public Category(string name)
+        => Name = name;
+}
