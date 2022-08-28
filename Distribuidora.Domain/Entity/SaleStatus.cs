1087d45da8b351098833c93289f1c98e9f649260
welberoliveira <welberoliveira3@gmail.com>
2022-08-28 00:06:37 -0300

    Criação da classe SaleStatus e teste unitário resolve #61

---
+++ Distribuidora.Domain/Entity/SaleStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace Distribuidora.Domain.Entity;
+public class SaleStatus
+{
+    public static Guid Id = Guid.NewGuid();
+    public double Budget { get; set; }
+    public int Issued { get; set; }
+    public int Canceled { get; set; }
+
+    public SaleStatus(double budget, int issued, int canceled)
+    {
+        Budget = budget;
+        Issued = issued;
+        Canceled = canceled;
+    }
+}
