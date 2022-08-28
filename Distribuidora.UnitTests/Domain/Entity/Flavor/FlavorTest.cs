5fdfacaac8d8ba0235679afc1f87c2f402ac487b
welberoliveira <welberoliveira3@gmail.com>
2022-08-27 23:05:39 -0300

    Criação da classe Flavor e teste unitário resolve #48

---
+++ Distribuidora.UnitTests/Domain/Entity/Flavor/FlavorTest.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using DomainEntity = Distribuidora.Domain.Entity;
+using FluentAssertions;
+
+namespace Distribuidora.UnitTests.Domain.Entity.Flavor;
+public class FlavorTest
+{
+    [Fact(DisplayName = nameof(Instaciate))]
+    [Trait("Domain", "Flavor")]
+    public void Instaciate()
+    {
+        //Arrange
+        int sweetened = 0;
+        int bitter = 1;
+        int fruity = 2;
+        int smooth = 3;
+
+        //Act
+        var flavor = new DomainEntity.Flavor(sweetened, bitter, fruity, smooth);
+
+        //Assert
+        flavor.Should().NotBeNull();
+        flavor.Sweetened.Should().Be(sweetened);
+        flavor.Bitter.Should().Be(bitter);
+        flavor.Fruity.Should().Be(fruity);
+        flavor.Smooth.Should().Be(smooth);
+    }
+}
