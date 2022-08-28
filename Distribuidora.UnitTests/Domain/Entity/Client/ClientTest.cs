572f973b4687fa389719f3bc2d5629e6cae6d070
welberoliveira <welberoliveira3@gmail.com>
2022-08-27 21:21:55 -0300

    Criação da classe ClientTest

---
+++ Distribuidora.UnitTests/Domain/Entity/Client/ClientTest.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using DomainEntity = Distribuidora.Domain.Entity;
+using FluentAssertions;
+
+namespace Distribuidora.UnitTests.Domain.Entity.Client;
+public class ClientTest
+{
+    [Fact(DisplayName = nameof(Instaciate))]
+    [Trait("Domain", "Client")]
+    public void Instaciate()
+    {
+        //Arrange
+        String name = "José da Silva";
+        String cpfOrCnpj = "123.654.987-87";
+        String phone = "(38) 32144-6547";
+        String email = "teste@gmail.com";
+        String addressField = "Rua A";
+        String number = "1010";
+        String note = "Teste de obserção";
+        String zipCode = "39400000";
+        String stateName = "Minas Gerais";
+        String stateAbreviation = "MG";
+        String cityName = "Ubá";
+
+        //Act
+        var state = new DomainEntity.State(stateName, stateAbreviation);
+        var city = new DomainEntity.City(cityName, state);
+        var address = new DomainEntity.Address(addressField, number, note, zipCode, state, city);
+        var clientType = new DomainEntity.ClientType(1, 0);
+        var client = new DomainEntity.Client(name, cpfOrCnpj, phone, email, address, clientType);
+
+        //Assert
+        state.Should().NotBeNull();
+        city.Should().NotBeNull();
+        address.Should().NotBeNull();
+        client.Name.Should().Be(name);
+        client.CpfOrCnpj.Should().Be(cpfOrCnpj);
+        client.Phone.Should().Be(phone);
+        client.Email.Should().Be(email);
+        client.Address.Should().Be(address);
+        client.City.Should().Be(city);
+    }
+}
