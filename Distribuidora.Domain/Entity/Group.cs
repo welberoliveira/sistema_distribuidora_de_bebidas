f5887cac5b729ad800e532febc826258c0be4f5c
welberoliveira <welberoliveira3@gmail.com>
2022-09-14 13:26:50 -0300

    Criação do teste unitário para a entidade Group Criação da entidade Group resolve #31

---
+++ Distribuidora.Domain/Entity/Group.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Distribuidora.Domain.Entity;
+public class Group
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Permission Permission { get; set; }
+
+    public Group(string name, Permission permission)
+    {
+        Name = name;
+        Permission = permission;
+    }
+}
---
+++ Distribuidora.UnitTests/Domain/Entity/Group/GroupTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using DomainEntity = Distribuidora.Domain.Entity;
+using FluentAssertions;
+
+namespace Distribuidora.UnitTests.Domain.Entity.Group;
+public class GroupTest
+{
+    [Fact(DisplayName = nameof(Instaciate))]
+    [Trait("Domain", "Group")]
+    public void Instaciate()
+    {
+        //Arrange
+        String groupName = "Gestores";
+        String permissionName = "Administrador";
+
+        //Act
+        var permission = new DomainEntity.Permission(permissionName);
+        var group = new DomainEntity.Group(groupName, permission);
+
+        //Assert
+        permission.Should().NotBeNull();
+        group.Should().NotBeNull();
+        group.Name.Should().Be(groupName);
+    }
+}
