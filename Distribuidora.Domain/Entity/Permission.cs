048e9e93f67732fc0a2442294dbd30914a11b798
welberoliveira <welberoliveira3@gmail.com>
2022-09-14 13:16:53 -0300

    Criação do teste unitário da entidade Permission Criação da entidade Permission resolve #30

---
+++ Distribuidora.Domain/Entity/Permission.cs
@@ -0,0 +1,9 @@
+﻿namespace Distribuidora.Domain.Entity;
+public class Permission
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+
+    public Permission(string name)
+        => Name = name;
+}
---
+++ Distribuidora.UnitTests/Domain/Entity/Permission/PermissionTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using DomainEntity = Distribuidora.Domain.Entity;
+using FluentAssertions;
+
+namespace Distribuidora.UnitTests.Domain.Entity.Permission;
+public class PermissionTest
+{
+    [Fact(DisplayName = nameof(Instaciate))]
+    [Trait("Domain", "Permission")]
+    public void Instaciate()
+    {
+        //Arrange
+        String Name = "Administrador";
+
+        //Act
+        var state = new DomainEntity.Permission(Name);
+
+        //Assert
+        state.Should().NotBeNull();
+        state.Name.Should().Be(Name);
+    }
+}
